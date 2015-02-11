11900c025cf24ebc95d1ef4277405a2d610b44f8
psadlo <psadlo@kartgis.com.pl>
2015-02-11 23:58:24 +0100

    Pomocniczy nagłówek zawierający nagłówki openGL w odpowiedniej kolejności. Inne pliki powinny załączać właśnie jego.

---
+++ src/utils/OpenGLInclude.h
@@ -0,0 +1,18 @@
+/*
+ * OpenGLInclude.h
+ *
+ *  Created on: 5 wrz 2014
+ *      Author: Przemo
+ */
+
+#ifndef OPENGLINCLUDE_H_
+#define OPENGLINCLUDE_H_
+
+#define GLEW_STATIC
+#include <GL/glew.h> // musi być przed innymi bibliotekami OpenGL
+
+#include <GL/freeglut.h>
+
+
+
+#endif /* OPENGLINCLUDE_H_ */
