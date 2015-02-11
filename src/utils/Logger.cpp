6b4a31cd9aacfec872a2b15a07142521075eb44d
psadlo <psadlo@kartgis.com.pl>
2015-02-11 23:56:38 +0100

    Przeniesienie loggera do folderu utils.

---
+++ src/utils/Logger.cpp
@@ -0,0 +1,91 @@
+/*
+ * Debugger.cpp
+ *
+ *  Created on: 4 lut 2014
+ *      Author: Przemo
+ */
+
+#include "Logger.h"
+#include "../utils/OpenGLInclude.h"
+#include <cstdio>
+
+using namespace std;
+
+Logger::Logger() {
+	// TODO Auto-generated constructor stub
+
+	guiTextLines.assign(10, "");
+
+}
+
+Logger::~Logger() {
+}
+
+void Logger::printOnScreen() {
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0.0, glutGet(GLUT_WINDOW_WIDTH), 0.0, glutGet(GLUT_WINDOW_HEIGHT));
+
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+
+	glColor3f(0.0, 1.0, 0.0); // Green
+
+	int lineNumber = 1;
+	void * font = GLUT_BITMAP_9_BY_15;
+	for(vector<string>::iterator v_i = guiTextLines.begin(); v_i != guiTextLines.end(); ++v_i, ++lineNumber) {
+		glRasterPos2i(10, glutGet(GLUT_WINDOW_HEIGHT) - lineNumber*15);
+		string line = *v_i;
+		for(string::iterator s_i = line.begin(); s_i != line.end(); ++s_i) {
+			char c = *s_i;
+			glutBitmapCharacter(font, c);
+		}
+	}
+
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+
+}
+//
+//Where font is one of GLUT font constants:
+//
+//GLUT_BITMAP_8_BY_13
+//GLUT_BITMAP_9_BY_15
+//GLUT_BITMAP_TIMES_ROMAN_10
+//GLUT_BITMAP_TIMES_ROMAN_24
+//GLUT_BITMAP_HELVETICA_10
+//GLUT_BITMAP_HELVETICA_12
+//GLUT_BITMAP_HELVETICA_18
+
+void Logger::addLineToScreen(int lineNumber, char* format, ...) {
+		va_list args;	//  Variable argument list
+		int len;		//	String length
+		char * text;	//	Text
+
+		//  Initialize a variable argument list
+		va_start(args, format);
+
+		//  Return the number of characters in the string referenced the list of arguments.
+		//  _vscprintf doesn't count terminating '\0' (that's why +1)
+		len = _vscprintf(format, args) + 1;
+
+		//  Allocate memory for a string of the specified size
+		text = (char *)malloc(len * sizeof(char));
+
+		//  Write formatted output using a pointer to the list of arguments
+		vsnprintf(text, len, format, args);
+
+		//  End using variable argument list
+		va_end(args);
+
+		guiTextLines[lineNumber] = text;
+
+		//  Free the allocated memory for the string
+		free(text);
+}
---
+++ src/utils/Logger.h
@@ -0,0 +1,39 @@
+/*
+ * Debugger.h
+ *
+ *  Created on: 4 lut 2014
+ *      Author: Przemo
+ */
+
+#ifndef DEBUGGER_H_
+#define DEBUGGER_H_
+
+#include <string>
+#include <vector>
+
+class Logger {
+public:
+
+    static Logger& getInstance() {
+        static Logger instance; // Guaranteed to be destroyed.
+                              // Instantiated on first use.
+        return instance;
+    }
+
+    void printOnScreen();
+    void addLineToScreen(int lineNumber, char* format, ...);
+
+private:
+
+    Logger();                   // Constructor? (the {} brackets) are needed here.
+    // Dont forget to declare these two. You want to make sure they
+    // are unaccessable otherwise you may accidently get copies of
+    // your singleton appearing.
+    Logger(Logger const&);              // Don't Implement
+    void operator=(Logger const&); // Don't implement
+	virtual ~Logger(); //TODO potrzebny?
+
+	std::vector<std::string> guiTextLines;
+};
+
+#endif /* DEBUGGER_H_ */
