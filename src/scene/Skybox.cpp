a6a6010ce432f6c5ac2b076d188fdcbced0a41e6
psadlo <psadlo@kartgis.com.pl>
2015-02-11 23:49:38 +0100

    Optymalizacja rysowania siatki podłoża.

--- src/scene/Skybox.cpp
+++ src/scene/Skybox.cpp
@@ -6,12 +6,10 @@
  */
 
 #include "Skybox.h"
-
-#include <GL/freeglut.h>
+#include "../utils/OpenGLInclude.h"
 
 Skybox::Skybox() {
-	// TODO Auto-generated constructor stub
-	worldSize = 100;
+	worldSize = 30;
 	floorLevel = 0;
 	skyLevel = worldSize;
 	leftWall = -worldSize/2;
@@ -21,110 +19,23 @@ Skybox::Skybox() {
 }
 
 Skybox::~Skybox() {
-	// TODO Auto-generated destructor stub
 }
 
 void Skybox::draw() {
 	glMatrixMode(GL_MODELVIEW);
-	// TODO na razie rysujê bia³¹ siatkê na pod³odze, docelowo oteksturowaæ skybox
     glPushMatrix();
 
     int gridSpacing = 1;
-    glColor3f(1, 1, 1);
+    glColor3f(0.3, 0.3, 0.3);
 
+    glBegin(GL_LINES);
     for(int i = leftWall; i <= rightWall; i += gridSpacing) {
-        glBegin(GL_LINES);
         glVertex3f(i, 0, leftWall);
         glVertex3f(i, 0, rightWall);
-        glEnd();
-        glBegin(GL_LINES);
         glVertex3f(backWall, 0, i);
         glVertex3f(frontWall, 0, i);
-        glEnd();
     }
+    glEnd();
 
     glPopMatrix();
-
-
-
-    //void displaySkyBox()
-    //{
-    ////	glMatrixMode(GL_MODELVIEW);
-    ////	glEnable (GL_TEXTURE_2D);
-    ////
-    ////	//podloga
-    ////	glBindTexture (GL_TEXTURE_2D, woodTexture);
-    ////	glPushMatrix();
-    ////	glBegin(GL_QUADS);
-    ////	glTexCoord2f(0.0f, 0.0f);
-    ////	glVertex3f(-WORLD_BORDER, FLOOR_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, 0.0f);
-    ////	glVertex3f(WORLD_BORDER, FLOOR_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, FLOOR_REPEAT);
-    ////	glVertex3f(WORLD_BORDER, FLOOR_LVL, -WORLD_BORDER);
-    ////	glTexCoord2f(0.0f, FLOOR_REPEAT);
-    ////	glVertex3f(-WORLD_BORDER, FLOOR_LVL, -WORLD_BORDER);
-    ////	glEnd();
-    ////	glPopMatrix();
-    ////
-    ////	//sciany
-    ////	glBindTexture (GL_TEXTURE_2D, wallTexture);
-    ////	glPushMatrix();
-    ////	glBegin(GL_QUADS);
-    ////	//lewa
-    ////	glTexCoord2f(0.0f, 0.0f);
-    ////	glVertex3f(-WORLD_BORDER, FLOOR_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, 0.0f);
-    ////	glVertex3f(-WORLD_BORDER, FLOOR_LVL, -WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, WALL_REPEAT);
-    ////	glVertex3f(-WORLD_BORDER, TOP_LVL, -WORLD_BORDER);
-    ////	glTexCoord2f(0.0f, WALL_REPEAT);
-    ////	glVertex3f(-WORLD_BORDER, TOP_LVL, WORLD_BORDER);
-    ////	//prawa
-    ////	glTexCoord2f(0.0f, 0.0f);
-    ////	glVertex3f(WORLD_BORDER, FLOOR_LVL, -WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, 0.0f);
-    ////	glVertex3f(WORLD_BORDER, FLOOR_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, WALL_REPEAT);
-    ////	glVertex3f(WORLD_BORDER, TOP_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(0.0f, WALL_REPEAT);
-    ////	glVertex3f(WORLD_BORDER, TOP_LVL, -WORLD_BORDER);
-    ////	//przednia
-    ////	glTexCoord2f(0.0f, 0.0f);
-    ////	glVertex3f(WORLD_BORDER, FLOOR_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, 0.0f);
-    ////	glVertex3f(-WORLD_BORDER, FLOOR_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, WALL_REPEAT);
-    ////	glVertex3f(-WORLD_BORDER, TOP_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(0.0f, WALL_REPEAT);
-    ////	glVertex3f(WORLD_BORDER, TOP_LVL, WORLD_BORDER);
-    ////	//tylna
-    ////	glTexCoord2f(0.0f, 0.0f);
-    ////	glVertex3f(-WORLD_BORDER, FLOOR_LVL, -WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, 0.0f);
-    ////	glVertex3f(WORLD_BORDER, FLOOR_LVL, -WORLD_BORDER);
-    ////	glTexCoord2f(FLOOR_REPEAT, WALL_REPEAT);
-    ////	glVertex3f(WORLD_BORDER, TOP_LVL, -WORLD_BORDER);
-    ////	glTexCoord2f(0.0f, WALL_REPEAT);
-    ////	glVertex3f(-WORLD_BORDER, TOP_LVL, -WORLD_BORDER);
-    ////	glEnd();
-    ////	glPopMatrix();
-    ////
-    ////	//sufit
-    ////	glBindTexture (GL_TEXTURE_2D, ceilingTexture);
-    ////	glPushMatrix();
-    ////	glBegin(GL_QUADS);
-    ////	glTexCoord2f(0.0f, 0.0f);
-    ////	glVertex3f(-WORLD_BORDER, TOP_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(CEILING_REPEAT, 0.0f);
-    ////	glVertex3f(WORLD_BORDER, TOP_LVL, WORLD_BORDER);
-    ////	glTexCoord2f(CEILING_REPEAT, CEILING_REPEAT);
-    ////	glVertex3f(WORLD_BORDER, TOP_LVL, -WORLD_BORDER);
-    ////	glTexCoord2f(0.0f, CEILING_REPEAT);
-    ////	glVertex3f(-WORLD_BORDER, TOP_LVL, -WORLD_BORDER);
-    ////	glEnd();
-    ////	glPopMatrix();
-    ////
-    ////	glDisable (GL_TEXTURE_2D);
-    //}
 }
